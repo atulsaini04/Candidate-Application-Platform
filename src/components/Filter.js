676db85814f744ec0f6bcb7725a43612bf14e2a2
Atul <satulnet2@gmail.com>
2024-05-05 01:34:39 +0530

    Filteradded

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,14 +1,35 @@
 // src/components/Filter.js
 import React from 'react';
-import { TextField, Button, FormControl, Select, MenuItem, InputLabel } from '@material-ui/core';
+import { TextField, Button, Grid } from '@material-ui/core';
 
 const Filter = ({ filters, handleChange, handleApply }) => {
   return (
-    <div>
-      {/* <TextField label="Minimum Experience" type="number" value={filters.minExperience} onChange={handleChange('minExperience')} />
-      {/* Other filter inputs */}
-      {/* <Button variant="contained" color="primary" onClick={handleApply}>Apply</Button> */} 
-    </div>
+    <Grid container spacing={2} direction="column">
+      <Grid item>
+        <TextField
+          label="Company Name"
+          value={filters.companyName}
+          onChange={handleChange('companyName')}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          label="Job Role"
+          value={filters.jobRole}
+          onChange={handleChange('jobRole')}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          label="Location"
+          value={filters.location}
+          onChange={handleChange('location')}
+        />
+      </Grid>
+      <Grid item>
+        <Button variant="contained" color="primary" onClick={handleApply}>Apply</Button>
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/JobListingsPage.js
+++ src/pages/JobListingsPage.js
@@ -1,46 +1,58 @@
 // src/pages/JobListingsPage.js
-import React, { useEffect } from 'react';
-import { Grid } from '@material-ui/core';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchJobListingsRequest, fetchJobListingsSuccess, fetchJobListingsFailure } from '../actions/jobListingActions.js';
+import React, { useState, useEffect } from 'react';
+import { Grid, TextField, Button } from '@material-ui/core';
+import axios from 'axios';
 import JobCard from '../components/JobCard';
 import Filter from '../components/Filter';
-import axios from 'axios';
 
 const JobListingsPage = () => {
-  const dispatch = useDispatch();
-  const { jobListings, loading, error } = useSelector(state => state.jobListings);
+  const [jobs, setJobs] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState([]);
+  const [filters, setFilters] = useState({
+    companyName: '',
+    jobRole: '',
+    location: ''
+    // Add other filter fields here
+  });
+
+  const handleChange = (field) => (event) => {
+    setFilters({ ...filters, [field]: event.target.value });
+  };
+
+  const handleApplyFilters = () => {
+    const filtered = jobs.filter(job => {
+      for (const key in filters) {
+        if (filters[key] !== '' && String(job[key]).toLowerCase().indexOf(filters[key].toLowerCase()) === -1) {
+          return false;
+        }
+      }
+      return true;
+    });
+    setFilteredJobs(filtered);
+  };
 
   useEffect(() => {
-    dispatch(fetchJobListingsRequest());
     fetchJobs();
-  }, [dispatch]);
+  }, []);
 
   const fetchJobs = async () => {
     try {
       const response = await axios.post('https://api.weekday.technology/adhoc/getSampleJdJSON');
-      dispatch(fetchJobListingsSuccess(response.data.jdList)); // Assuming response.data is an array of job objects
+      setJobs(response.data.jdList);
+      setFilteredJobs(response.data.jdList);
     } catch (error) {
-      dispatch(fetchJobListingsFailure(error.message));
+      console.error('Error fetching job listings:', error);
     }
   };
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={3}>
-        <Filter />
+        <Filter filters={filters} handleChange={handleChange} handleApply={handleApplyFilters} />
       </Grid>
       <Grid item xs={9}>
         <Grid container spacing={2}>
-          {jobListings.map((job) => (
+          {filteredJobs.map((job) => (
             <Grid item xs={12} key={job.jdUid}>
               <JobCard job={job} />
             </Grid>
